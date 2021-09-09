9f0aa292f81f37824cc20e0d60c819e21902e9c5
sophiekdj <s.dejong057@gmail.com>
2021-09-09 13:25:02 +0800

    Created variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,87 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create variables
+var passwordLength;
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmNumbers;
+var confirmSymbols;
+
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// Use lowerCase array to create upperCase array
+var upperCase = lowerCase.map(function (toUpper) {
+  return toUpper.toUpperCase();
+});
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = [
+  "!",
+  " ",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  ":",
+  ";",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
