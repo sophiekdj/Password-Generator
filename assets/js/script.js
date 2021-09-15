e1e3c9f61e65e06fb5f28b81db851b6b8cf05bad
sophiekdj <s.dejong057@gmail.com>
2021-09-15 20:57:25 +0800

    Condensed else if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,28 +86,27 @@ function generatePassword() {
   console.log("Password length of " + passwordLength);
 
   if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt(
-      "The password length chosen must be between 8 and 128"
-    );
+    window.alert("You haven't chosen a password length between 8 & 128");
     console.log("Password length of " + passwordLength);
+    return "";
   } else if (!passwordLength) {
-    ("You must choose a password length between 8 and 128");
-    generatePassword();
-  } else {
-    // Confirmation messages for use of lower case, upper case, numbers and symbols
-    confirmLowerCase = confirm(
-      "Would you like lower case alphabet characters in your password?"
-    );
-    console.log("Lower case " + confirmLowerCase);
-    confirmUpperCase = confirm(
-      "Would you like upper case alphabet characters in your password?"
-    );
-    console.log("Upper case " + confirmUpperCase);
-    confirmNumbers = confirm("Would you like numbers in your password?");
-    console.log("Numbers " + confirmNumbers);
-    confirmSymbols = confirm("Would you like symbols in your password?");
-    console.log("Symbols " + confirmSymbols);
+    window.alert("You must choose a password length between 8 and 128");
+    return "";
   }
+
+  // Confirmation messages for use of lower case, upper case, numbers and symbols
+  confirmLowerCase = confirm(
+    "Would you like lower case alphabet characters in your password?"
+  );
+  console.log("Lower case " + confirmLowerCase);
+  confirmUpperCase = confirm(
+    "Would you like upper case alphabet characters in your password?"
+  );
+  console.log("Upper case " + confirmUpperCase);
+  confirmNumbers = confirm("Would you like numbers in your password?");
+  console.log("Numbers " + confirmNumbers);
+  confirmSymbols = confirm("Would you like symbols in your password?");
+  console.log("Symbols " + confirmSymbols);
 
   // 0 confirm options chosen
   if (
@@ -116,67 +115,21 @@ function generatePassword() {
     !confirmNumbers &&
     !confirmSymbols
   ) {
-    userChoices = alert(
-      "You must have characters to create a password. Please start again."
-    );
+    alert("You must have characters to create a password. Please start again.");
+    generatePassword();
   }
-  // 4 options chosen
-  else if (
-    confirmLowerCase &&
-    confirmUpperCase &&
-    confirmNumbers &&
-    confirmSymbols
-  ) {
-    userChoices = lowerCase.concat(upperCase, numbers, symbols);
-    console.log(userChoices);
+
+  if (confirmLowerCase) {
+    userChoices = userChoices.concat(lowerCase);
   }
-  // 3 options chosen
-  else if (confirmLowerCase && confirmUpperCase && confirmNumbers) {
-    userChoices = lowerCase.concat(upperCase, numbers);
-    console.log(userChoices);
-  } else if (confirmLowerCase && confirmUpperCase && confirmSymbols) {
-    userChoices = lowerCase.concat(upperCase, symbols);
-    console.log(userChoices);
-  } else if (confirmLowerCase && confirmNumbers && confirmSymbols) {
-    userChoices = lowerCase.concat(numbers, symbols);
-    console.log(userChoices);
-  } else if (confirmUpperCase && confirmNumbers && confirmSymbols) {
-    userChoices = upperCase.concat(numbers, symbols);
-    console.log(userChoices);
+  if (confirmUpperCase) {
+    userChoices = userChoices.concat(upperCase);
   }
-  // 2 options chosen
-  else if (confirmLowerCase && confirmUpperCase) {
-    userChoices = lowerCase.concat(upperCase);
-    console.log(userChoices);
-  } else if (confirmLowerCase && confirmNumbers) {
-    userChoices = lowerCase.concat(numbers);
-    console.log(userChoices);
-  } else if (confirmLowerCase && confirmSymbols) {
-    userChoices = lowerCase.concat(symbols);
-    console.log(userChoices);
-  } else if (confirmUpperCase && confirmNumbers) {
-    userChoices = upperCase.concat(numbers);
-    console.log(userChoices);
-  } else if (confirmUpperCase && confirmSymbols) {
-    userChoices = upperCase.concat(symbols);
-    console.log(userChoices);
-  } else if (confirmNumbers && confirmSymbols) {
-    userChoices = numbers.concat(symbols);
-    console.log(userChoices);
+  if (confirmNumbers) {
+    userChoices = userChoices.concat(numbers);
   }
-  // 1 option chosen
-  else if (confirmLowerCase) {
-    userChoices = lowerCase;
-    console.log(userChoices);
-  } else if (confirmUpperCase) {
-    userChoices = upperCase;
-    console.log(userChoices);
-  } else if (confirmNumbers) {
-    userChoices = numbers;
-    console.log(userChoices);
-  } else if (confirmSymbols) {
-    userChoices = symbols;
-    console.log(userChoices);
+  if (confirmSymbols) {
+    userChoices = userChoices.concat(symbols);
   }
 
   // Empty variable
